5ec8e3b4694a16ff853c4d1a62df252cdc74f3de
Lianjieshan <51137255+Lianjieshan@users.noreply.github.com>
2022-04-07 12:24:20 -0400

    feat：添加errorboundary

---
+++ jira/src/components/error-boundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type FallbackRender = (props: { error: Error | null }) => React.ReactElement;
+
+// react 自带处理errorBoundary的库
+// https://github.com/bvaughn/react-error-boundary
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  // 当子组件抛出异常，这里会接收到并且调用
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
