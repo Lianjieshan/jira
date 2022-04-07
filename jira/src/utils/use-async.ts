7fdf1e111af4567d0cc4d8e68bb93547e3b0d76f
Lianjieshan <51137255+Lianjieshan@users.noreply.github.com>
2022-04-07 11:59:15 -0400

    feat: 登录注册loading和错误处理

--- jira/src/utils/use-async.ts
+++ jira/src/utils/use-async.ts
@@ -1,62 +1,72 @@
 import { useState } from "react";
 
 interface State<D> {
-    error: Error | null;
-    data: D | null;
-    stat: 'idle' | 'loading' | 'error' | 'success'
+  error: Error | null;
+  data: D | null;
+  stat: "idle" | "loading" | "error" | "success";
 }
 
 const defaultInitialState: State<null> = {
-    stat: "idle",
-    data: null,
-    error: null,
+  stat: "idle",
+  data: null,
+  error: null,
+};
+
+const defaultConfig = {
+  throwOnError: false,
+};
+
+export const useAsync = <D>(
+  initialState?: State<D>,
+  initialConfig?: typeof defaultConfig
+) => {
+  const config = { ...defaultConfig, ...initialConfig };
+  const [state, setState] = useState<State<D>>({
+    ...defaultInitialState,
+    ...initialState,
+  });
+
+  const setData = (data: D) =>
+    setState({
+      data,
+      stat: "success",
+      error: null,
+    });
+
+  const setError = (error: Error) =>
+    setState({
+      error,
+      stat: "error",
+      data: null,
+    });
+
+  // run 用来触发异步请求
+  const run = (promise: Promise<D>) => {
+    if (!promise || !promise.then) {
+      throw new Error("请传入 Promise 类型数据");
+    }
+    setState({ ...state, stat: "loading" });
+    return promise
+      .then((data) => {
+        setData(data);
+        return data;
+      })
+      .catch((error) => {
+        // catch会消化异常，如果不主动抛出，外面是接收不到异常的
+        setError(error);
+        if (config.throwOnError) return Promise.reject(error);
+        return error;
+      });
   };
 
-  export const useAsync = <D>(initialState?: State<D>) => {
-    const [state, setState] = useState<State<D>>({
-      ...defaultInitialState,
-      ...initialState,
-    });
-  
-    const setData = (data: D) =>
-      setState({
-        data,
-        stat: "success",
-        error: null,
-      });
-  
-    const setError = (error: Error) =>
-      setState({
-        error,
-        stat: "error",
-        data: null,
-      });
-  
-    // run 用来触发异步请求
-    const run = (promise: Promise<D>) => {
-      if (!promise || !promise.then) {
-        throw new Error("请传入 Promise 类型数据");
-      }
-      setState({ ...state, stat: "loading" });
-      return promise
-        .then((data) => {
-          setData(data);
-          return data;
-        })
-        .catch((error) => {
-          setError(error);
-          return error;
-        });
-    };
-  
-    return {
-      isIdle: state.stat === "idle",
-      isLoading: state.stat === "loading",
-      isError: state.stat === "error",
-      isSuccess: state.stat === "success",
-      run,
-      setData,
-      setError,
-      ...state,
-    };
+  return {
+    isIdle: state.stat === "idle",
+    isLoading: state.stat === "loading",
+    isError: state.stat === "error",
+    isSuccess: state.stat === "success",
+    run,
+    setData,
+    setError,
+    ...state,
   };
+};
